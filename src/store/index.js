e14f38e66b0e803c570777f91e3c44a21da2866c
Maksym <68236759+Coldmidas@users.noreply.github.com>
2022-08-07 19:54:24 +0300

    add reduxToolkit and optimize code

--- src/store/index.js
+++ src/store/index.js
@@ -1,30 +1,45 @@
-import { createStore, combineReducers, compose } from 'redux';
+import { configureStore } from '@reduxjs/toolkit';
 import heroes from '../reducers/heroes';
 import filters from '../reducers/filters';
 
-const enhancer = (createStore) => (...args) => {
-    const store = createStore(...args);
+const stringMiddleware = () => (next) => (action) => {
+    if (typeof action === 'string') {
+        return next ({
+            type: action
+        })
+    }
+    return next(action)
+};
 
-    const oldDispatch = store.dispatch;
-    store.dispatch = (action) => {
-        if (typeof action === 'string') {
-            return oldDispatch ({
-                type: action
-            })
-        }
-        return oldDispatch(action)
-    } 
-    return store;
-}
+// const enhancer = (createStore) => (...args) => {
+//     const store = createStore(...args);
+
+//     const oldDispatch = store.dispatch;
+//     store.dispatch = (action) => {
+//         if (typeof action === 'string') {
+//             return oldDispatch ({
+//                 type: action
+//             })
+//         }
+//         return oldDispatch(action)
+//     } 
+//     return store;
+// }
 
 
 
-const store = createStore(
-    combineReducers({heroes, filters}),
-    compose(
-        enhancer,
-        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-    ));
+// const store = createStore(
+//     combineReducers({heroes, filters}),
+//     compose(applyMiddleware(Reduxthunk, stringMiddleware),
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+//     );
+
+const store = configureStore({
+    reducer: {heroes, filters},
+    middleware: GetDefaultMiddleware => GetDefaultMiddleware().concat(stringMiddleware),
+    devTools: process.env.NODE_ENV !== 'production'
+    
+})
 
 export default store;
 
